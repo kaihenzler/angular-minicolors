c70beacdd5ccad845be08bbea43d082134cdad8f
Kai Henzler <henzler@w11k.de>
2017-12-22 12:16:08 +0100

    updates gh-pages to release 0.0.11

--- angular-minicolors.js
+++ angular-minicolors.js
@@ -54,13 +54,37 @@
           return config;
         };
 
+        /**
+         * check if value is valid color value
+         * e.g.#fff000 or #fff
+         * @param color
+         */
+        function isValidColor(color) {
+          return /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(color);
+        }
+
+        function canSetValue() {
+          return (element.data('minicolors-settings') != null)
+        }
+
+        /**
+         * set color value as minicolors internal color value
+         * @param color
+         */
+        function setMinicolorsValue(color) {
+          if (isValidColor(color) && canSetValue()) {
+            element.minicolors('value', color);
+          }
+        }
+
         //what to do if the value changed
         ngModel.$render = function() {
+
 
           //we are in digest or apply, and therefore call a timeout function
           $timeout(function() {
             var color = ngModel.$viewValue;
-            element.minicolors('value', color);
+            setMinicolorsValue(color);
           }, 0, false);
         };
 
@@ -73,7 +97,8 @@
           var settings = getSettings();
           settings.change = function(hex) {
             scope.$apply(function() {
-              ngModel.$setViewValue(hex);
+              if (isValidColor(hex))
+                ngModel.$setViewValue(hex);
             });
           };
 
@@ -95,7 +120,7 @@
           if (!inititalized) {
             $timeout(function() {
               var color = ngModel.$viewValue;
-              element.minicolors('value', color);
+              setMinicolorsValue(color);
             }, 0);
             inititalized = true;
             return;
@@ -104,7 +129,8 @@
           function onBlur(e) {
             scope.$apply(function() {
                 var color = element.minicolors('value');
-                ngModel.$setViewValue(color);
+                if (isValidColor(color))
+                  ngModel.$setViewValue(color);
             });              
           }
         };
@@ -113,7 +139,16 @@
         //initital call
 
         // Watch for changes to the directives options and then call init method again
-        scope.$watch(getSettings, initMinicolors, true);
+        var unbindWatch = scope.$watch(getSettings, initMinicolors, true);
+
+        scope.$on('$destroy', function () {
+          if (element.hasClass('minicolors-input')) {
+              element.minicolors('destroy');
+              element.remove();
+          }
+          if (unbindWatch) unbindWatch();
+        });
+
       }
     };
   }]);
